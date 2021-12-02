4982dd84b5774c496892b69c63e58e527f49843a
Michael Moncera <michaelmoncera@gmail.com>
2021-12-01 19:25:30 -0800

    added event listener to update src attribute of the image when the photoURL value is inputed

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+
+var $photoUrl = document.getElementById('photo-url');
+var $image = document.querySelector('.image');
+
+$photoUrl.addEventListener('input', function (event) {
+  $image.src = event.target.value;
+});
