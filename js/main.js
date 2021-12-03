5db96d1d0c9ef61e2cab60cb40bf1bc13c4b9393
Michael Moncera <michaelmoncera@gmail.com>
2021-12-02 20:45:59 -0800

    added event listener for submit with all task list functionality

--- js/main.js
+++ js/main.js
@@ -7,3 +7,19 @@ var $image = document.querySelector('.image');
 $photoUrl.addEventListener('input', function (event) {
   $image.src = event.target.value;
 });
+
+var $form = document.querySelector('.form');
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var entry = {
+    title: $form.elements.title.value,
+    photoUrl: $form.elements.photoUrl.value,
+    notes: $form.elements.notes.value,
+    nextEntryId: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.unshift(entry);
+  $image.src = 'images/placeholder-image-square.jpg';
+  $form.reset();
+});
