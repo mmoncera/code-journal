e9792380d6fd8d32ebe44947b771be8b13e1297b
Michael Moncera <michaelmoncera@gmail.com>
2021-12-05 17:06:30 -0800

    completed issue-1-create-an-entry

--- js/data.js
+++ js/data.js
@@ -7,13 +7,13 @@ var data = {
   nextEntryId: 1
 };
 
-window.addEventListener('beforeunload', function (event) {
-  var dataJSON = JSON.stringify(data);
-  localStorage.setItem('javascript-local-storage', dataJSON);
-});
-
 var previousDataJSON = localStorage.getItem('javascript-local-storage');
 
 if (previousDataJSON !== null) {
   data = JSON.parse(previousDataJSON);
 }
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataJSON);
+});
