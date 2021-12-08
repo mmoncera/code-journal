b0028fdae92115eae17f3716ca9d8e92bbf7b80f
Michael Moncera <michaelmoncera@gmail.com>
2021-12-07 16:39:17 -0800

    defined a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML

--- js/main.js
+++ js/main.js
@@ -23,3 +23,52 @@ $form.addEventListener('submit', function (event) {
   $photoPreview.src = 'images/placeholder-image-square.jpg';
   $form.reset();
 });
+
+function renderEntry(entry) {
+  var $entry = document.createElement('li');
+  $entry.setAttribute('class', 'row entry');
+
+  var $entryPhotoContainer = document.createElement('div');
+  $entryPhotoContainer.setAttribute(
+    'class',
+    'column-half entry-photo-container'
+  );
+
+  var $entryPhoto = document.createElement('img');
+  $entryPhoto.setAttribute('class', 'photo-preview entry-photo');
+  $entryPhoto.setAttribute('src', entry.photoUrl);
+  $entryPhoto.setAttribute('alt', 'placeholder image');
+
+  var $entryInformationContainer = document.createElement('div');
+  $entryInformationContainer.setAttribute(
+    'class',
+    'column-half entry-information-container'
+  );
+
+  var $entryTitle = document.createElement('h2');
+  $entryTitle.setAttribute('class', 'entry-title');
+  $entryTitle.textContent = entry.title;
+
+  var $entryNotes = document.createElement('p');
+  $entryNotes.setAttribute('class', 'entry-notes');
+  $entryNotes.textContent = entry.notes;
+
+  $entry.append($entryPhotoContainer, $entryInformationContainer);
+  $entryPhotoContainer.appendChild($entryPhoto);
+  $entryInformationContainer.append($entryTitle, $entryNotes);
+
+  return $entry;
+}
+
+// Test to see if it displays in DOM
+var example = {
+  title: 'JavaScript',
+  photoUrl:
+    'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8amF2YXNjcmlwdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+  notes:
+    'This is challenging and fun. Learning a lot and looking forward to more!!!'
+};
+
+var $entriesContainer = document.querySelector('.entries-container');
+
+$entriesContainer.appendChild(renderEntry(example));
